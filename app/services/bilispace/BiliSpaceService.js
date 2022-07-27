5da532c304db103c39a694a8382cd933802600c3
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-28 06:59:02 +0800

    refactor: add UA
    (cherry picked from commit f7aa364ea2ddae4a018771bc1ecb108906092387)

--- app/services/bilispace/BiliSpaceService.js
+++ app/services/bilispace/BiliSpaceService.js
@@ -45,7 +45,8 @@ export const getBiliSpaceService = async (url, id, data) => {
     return null;
 }
 export const requestOption = {
-    proxy: false
+    proxy: false,
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
 }
 export const BiliHeadImgHeightPercentage = 1 / 6.4
 
