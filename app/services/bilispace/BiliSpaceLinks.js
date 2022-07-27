301212eae744c004720a181f2becbea93418a0bb
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-27 21:33:28 +0800

    refactor: add the fetch amount of biliSpace replies
    (cherry picked from commit 00383be0ecbe9409c0da6bbd199c3e40dfa7f81e)

--- app/services/bilispace/BiliSpaceLinks.js
+++ app/services/bilispace/BiliSpaceLinks.js
@@ -2,7 +2,7 @@ export const spaceApiUrl = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_s
 export const postPageUrl = "https://t.bilibili.com/"
 export const postApiUrl = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id="
 export const commentReplyApiUrl = "https://api.bilibili.com/x/v2/reply/reply?ps=20&oid="
-export const commentApiUrl = "https://api.bilibili.com/x/v2/reply?"
+export const commentApiUrl = "https://api.bilibili.com/x/v2/reply?ps=49&"
 export const mobileSpaceUrl = "https://m.bilibili.com/space/"
 export const PCSpaceRegUrl = "https://space.bilibili.com/.*/dynamic"
 export const userInfoApiUrl = "https://api.bilibili.com/x/web-interface/card?photo=true&mid="
