1b98462c2b26e396b0cf5fa222821b79f69a3b1e
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-27 16:49:15 +0800

    chore: rename app
    (cherry picked from commit c62f8671c867fb87088ffc671b4c6947affa54bb)

--- android/app/src/main/java/org/collector/MainActivity.java
+++ android/app/src/main/java/org/collector/MainActivity.java
@@ -1,4 +1,4 @@
-package com.collector;
+package org.collector;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
@@ -14,6 +14,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "collector";
+    return "Collector";
   }
 }
