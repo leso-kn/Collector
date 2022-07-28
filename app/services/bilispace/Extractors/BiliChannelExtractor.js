30585646ceb77ea117199c376d5edf2396c2ef2b
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-28 19:26:53 +0800

    refactor: add height estimate
    (cherry picked from commit 31e314bfdde7106c721c42e062cb7a0c58b0258a)

--- app/services/bilispace/Extractors/BiliChannelExtractor.js
+++ app/services/bilispace/Extractors/BiliChannelExtractor.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {requestOption} from "../BiliSpaceService";
 import {mobileSpaceUrl, postPageUrl, spaceApiUrl, trendingApiUrl} from "../BiliSpaceLinks";
+import rnTextSize, {TSFontSpecs} from 'react-native-text-size'
+import {Dimensions} from "react-native";
 
 export class BiliChannelExtractor {
     data
@@ -55,29 +57,52 @@ export class BiliChannelExtractor {
     getHeadImgRatio() {
         return 1 / 6.4
     }
+
     getPosts(pn, lastID) {
         let randomID;
-        !this.card && (randomID= this.url.split("fake_uid=")[1].split('&')[0])
+        !this.card && (randomID = this.url.split("fake_uid=")[1].split('&')[0])
         return axios.get(this.card ? spaceApiUrl + this.getIdentifyName() + `&offset_dynamic_id=${lastID}`
             : this.url + `&hot_offset=${lastID}`).then(res => {
                 let result = res.data.data.cards || []
                 result.hasMore = () => res.data.data.has_more
                 result.getLastID = () => res.data.data.cards[res.data.data.cards.length - 1].desc.dynamic_id_str
-                for(let item of result){
+                for (let item of result) {
+                    item.getIdentifyID = ()=> item.desc.dynamic_id_str
+                    item.getHeight = ()=>{
+                        let init = 148
+                        let type = item.desc.type
+                        let contentCard = JSON.parse(item.card)
+                        let forwardCard
+                        if (type === 1) {
+                            init += 165
+                            forwardCard = JSON.parse(contentCard.origin)
+                            let returnType = item.desc.origin.type
+                            if([8, 64, 2048, 4200].includes(returnType))init += 340
+                            if(returnType === 4200 && forwardCard.title) init += 33
+                            if(returnType === 2 && forwardCard.item.pictures.length) init += 210
+                        }
+                        if([8, 64, 2048, 4200].includes(type))init += 340
+                        if(type === 2 && contentCard.item.pictures.length) init += 210
+                        if(type === 4200 && contentCard.title) init += 33
+                        return init
+                    }
                     item.url = postPageUrl + item.desc.dynamic_id_str
-                    item.getTime = ()=>item.desc.timestamp * 1000
+                    item.getTime = () => item.desc.timestamp * 1000
                 }
                 return result
             }
         )
     }
-    getUrl(){
+
+    getUrl() {
         return mobileSpaceUrl + this.getIdentifyName()
     }
-    getServicePrefix(){
+
+    getServicePrefix() {
         return "biliSpace"
     }
-    getIdentifyID(){
-        return this.getServicePrefix() +  this.getIdentifyName()
+
+    getIdentifyID() {
+        return this.getServicePrefix() + this.getIdentifyName()
     }
 }
