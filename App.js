1b0ffcb6599ecfdef62ba3c96addff3a1d4e652d
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-30 06:58:06 +0800

    UI: improve animation for page navigation
    (cherry picked from commit a413f426bf2d1eeaddfa02d280adb38554de43ab)

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useEffect} from 'react';
 import Index from './app/index.js'
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
@@ -14,17 +6,16 @@ import Channel from "./app/components/channel";
 import FullPost from "./app/components/fullPost";
 import {Search} from "./app/components/search";
 import {Group} from "./app/components/Group";
-import {LogBox} from "react-native";
 import {getTheme} from "./app/utils";
 
 const Stack = createNativeStackNavigator();
-// LogBox.ignoreAllLogs();
 
 const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{
-                headerStyle: {backgroundColor: getTheme().color, elevation: 0}
+                headerStyle: {backgroundColor: getTheme().color, elevation: 0},
+                animation:"fade"
             }}>
                 <Stack.Screen name="Home" component={Index} options={{
                     headerShadowVisible: false
