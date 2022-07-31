1cb1de85c62b212866c1f70b96590f7ad966c4a3
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-31 12:18:33 +0800

    refactor: change implements related to Flatlist(fullPost.js)
    (cherry picked from commit daede5bbeb0f88e6759b87e4bbd79d19db73e0ac)

--- app/components/fullPost.js
+++ app/components/fullPost.js
@@ -1,45 +1,65 @@
 import Post from "./post";
 import React, {useEffect, useReducer, useRef, useState} from 'react';
 import {FlatList, View} from "react-native";
-import {FIRST_POST, HOT_FIRST, OLD_FIRST, OTHER_POST} from "../constants";
+import {deviceWidth, FIRST_POST, HOT_FIRST, OLD_FIRST, OTHER_POST} from "../constants";
 import {findService} from "../findService";
 
 const FullPost = (packedProps) => {
     let props = packedProps.route.params
-
+    const parentRef = useRef({IDList: []});
     const reducer = (state, action) => {
         // console.log(JSON.stringify(state.map(x=>x.member.uname+" " +x.content.message)) + "state"+ state.length)
         // console.log(JSON.stringify(action.data.map(x=>x.member.uname+" " +x.content.message)) + "action" + action.data.length)
-        return [...state, ...action.data]
+        let result = [...state]
+        for(let item of action.data){
+           if(result.map(x=>x.getIdentifyID()).includes(item.getIdentifyID())) continue
+            result.push(item)
+        }
+        return result
     }
     const [pn, setPn] = useState(1)
     const [hasMore, setHasMore] = useState(true)
     const [comments, dispatch] = useReducer(reducer, [])
     const [sortType, setSortType] = useState(HOT_FIRST)
     const [randomID, doUpdate] = useState(0)
-    const parentRef = useRef({});
+    const layoutMap = useRef(new Map())
 
     useEffect(() => {
-        findService(props.url, props.id ,props.data).then(res => {
+        findService(props.url, props.id, props.data).then(res => {
             parentRef.current.parentID = res.getID()
             parentRef.current.parentType = res.getType()
             return res.getComments(pn)
         }).then(res => {
             //TODO: show loading when fetching
-          //  alert(JSON.stringify(res.data.data.replies))
-            res?.data?.data?.replies?.length && dispatch({data: res.data.data.replies})
-            setHasMore(res?.data?.data?.replies?.length)
+            //  alert(JSON.stringify(res.data.data.replies))
+            res.length && dispatch({data: res})
+            setHasMore(res.hasMore())
         })
     }, [pn])
     const head = (
         <View>
             <View style={{marginTop: 5, marginBottom: 10, borderBottomColor: "gray", borderBottomWidth: 0.3}}>
-                <Post type={FIRST_POST} data={props.data} depth={0} url={props.url} id={props.id} navigation={packedProps.navigation} />
+                <Post type={FIRST_POST} data={props.data} depth={0} url={props.url} id={props.id}
+                      navigation={packedProps.navigation}/>
             </View>
         </View>
     )
     const renderFunc = (comment) => {
-        return (<Post navigation={packedProps.navigation} data={comment.item} parentType={parentRef.current.parentType} parentID={parentRef.current.parentID} type={OTHER_POST} depth={0} url={"biliComment"}/>)
+        return (<Post navigation={packedProps.navigation} onLayout={e => {
+            if (e.nativeEvent.layout.width !== deviceWidth) return
+            let tempID = comment.item.getIdentifyID()
+            let item = layoutMap.current.get(tempID)
+            if (!item || (item && item.height < e.nativeEvent.layout.height)) {
+                let offsetValue = 0
+                for (let i of layoutMap.current) {
+                    if (i[0] === tempID) break
+                    offsetValue += i[1].height + 0.4
+                }
+                layoutMap.current.set(tempID, {height: e.nativeEvent.layout.height, offset: offsetValue})
+            }
+        }} data={comment.item} parentType={parentRef.current.parentType} parentID={parentRef.current.parentID}
+                      height={layoutMap.current.get(comment.item.getIdentifyID())?.height > 121?layoutMap.current.get(comment.item.getIdentifyID())?.height : undefined}
+                      type={OTHER_POST} depth={0} url={"biliComment"}/>)
     }
 
     return (
@@ -47,11 +67,21 @@ const FullPost = (packedProps) => {
                   ListHeaderComponent={head}
                   ListEmptyComponent={<View/>}
                   extraData={randomID}
-                  keyExtractor={(x, index)=> {
-                      return x.toString() + index
+                  keyExtractor={x => x.getIdentifyID()}
+                  onEndReached={() => {
+                      console.log(pn)
+                      hasMore && setPn(pn + 1)
                   }}
-                  onEndReached={() => hasMore && setPn(pn + 1)}
                   onEndReachedThreshold={0.1}
+                  getItemLayout={(data, index)=>{
+                      let item = layoutMap.current.get(data[index].getIdentifyID())
+                      if(!item)return undefined
+                      return {length: item.height, offset: item.offset, index}
+                  }}
+                  maxToRenderPerBatch={50}
+                  windowSize={8}
+                  initialNumToRender={8}
+                  bounces={false}
                   ItemSeparatorComponent={() => (<View style={{backgroundColor: "#dad7d7", height: 0.4}}/>)}
         />
     )
