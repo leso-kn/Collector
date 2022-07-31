1ecd81756fd1ac4e33b25cd6b276105899737a8f
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-07-31 17:00:43 +0800

    fix: null value
    (cherry picked from commit d6f4896286694bf71ba54e0185a243222241d5eb)

--- app/components/Feeds.js
+++ app/components/Feeds.js
@@ -6,7 +6,7 @@ export const Feeds = ({navigation, randomID}) => {
     const [urls, setUrls] = useState(null)
     useEffect(() => {
         AsyncStorage.getItem("subscriptionData").then(res => {
-            setUrls(JSON.parse(res).feeds.map(x => x.url))
+            setUrls(JSON.parse(res)?.feeds?.map(x => x.url))
         })
     }, [randomID])
     return urls ? <Posts urls={urls} navigation={navigation} randomID={randomID} sort={1}/> : null
