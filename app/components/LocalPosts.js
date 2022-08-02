0e853df0068e5b040df3a7c84672bc7eee1ddff7
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2022-08-02 10:10:38 +0800

    refactor: enable layoutMap for LocalPosts
    (cherry picked from commit 0de05eddeefefe2a5473ee04de9445f124d6951a)

--- app/components/LocalPosts.js
+++ app/components/LocalPosts.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useReducer, useRef, useState} from 'react';
-import {PREVIEW_POST} from "../constants";
+import {deviceWidth, PREVIEW_POST} from "../constants";
 import {FlatList, Text, View} from "react-native";
 import Post from "./post";
 
@@ -11,10 +11,9 @@ export const LocalPosts = (props) => {
         }
         return [...state, ...action.data]
     }
-    const [pn, setPn] = useState(1)
     const [randomID, doUpdate] = useState(0)
     const [users, dispatch] = useReducer(reducer, [])
-    const refContainer = useRef({})
+    const layoutMap = useRef(new Map())
 
     useEffect(() => {
         props.data && dispatch({data: props.data, type: "replace"})
@@ -22,18 +21,39 @@ export const LocalPosts = (props) => {
 
     const renderFunc = (post) => {
         return (
-            <Post id={"defaultPost"} parentID={post.item.parentID} parentType={post.item.parentType} type={PREVIEW_POST}
-                  depth={0} url={post.item.url} data={post.item} navigation={props.navigation}/>)
+            <Post id={"defaultPost"} parentID={post.item.parentID} parentType={post.item.parentType}
+                  type={PREVIEW_POST} depth={0} url={post.item.url} data={post.item}
+                  onLayout={e => {
+                      if (e.nativeEvent.layout.width !== deviceWidth) return
+                      let tempID = post.item.identifyID
+                      let item = layoutMap.current.get(tempID)
+                      if (!item || (item && item.height < e.nativeEvent.layout.height)) {
+                          let offsetValue = 0
+                          for (let i of layoutMap.current) {
+                              if (i[0] === tempID) break
+                              offsetValue += i[1].height + 0.4
+                          }
+                          layoutMap.current.set(tempID, {height: e.nativeEvent.layout.height, offset: offsetValue})
+                      }
+                  }}
+                  height={layoutMap.current.get(post.item.identifyID)?.height > 121 ? layoutMap.current.get(post.item.identifyID)?.height : undefined}
+                  navigation={props.navigation}/>)
     }
     return (
         <View style={{flex: 1}}>
             <FlatList data={users} renderItem={renderFunc} ListFooterComponent={(<View style={{height: 50}}/>)}
-                      keyExtractor={(item, index) => item.identifyID}
-                      onEndReached={() => {
-                          refContainer.current.hasMore && setPn(pn + 1)
-                      }}
+                      keyExtractor={(item) => item.identifyID}
                       extraData={randomID}
-                      onEndReachedThreshold={0.1}
+                      removeClippedSubviews={true}
+                      initialNumToRender={5}
+                      getItemLayout={(data, index) => {
+                          let item = layoutMap.current.get(data[index].identifyID)
+                          if (!item) return undefined
+                          return {length: item.height, offset: item.offset, index}
+                      }}
+                      maxToRenderPerBatch={40}
+                      windowSize={6}
+                      bounces={false}
                       ItemSeparatorComponent={() => (<View style={{backgroundColor: "#dad7d7", height: 0.4}}/>)}
             />
         </View>
