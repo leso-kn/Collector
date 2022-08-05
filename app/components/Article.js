ef7496ae2e87f85a930e9221ec697f2e0d872c59
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2022-08-05 16:59:58 +0800

    refactor: improve article
    (cherry picked from commit 0f417ab8441c735a475760d226b72df2e9937c3a)

--- app/components/Article.js
+++ app/components/Article.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import {ScrollView, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {FlatList, ScrollView, View} from "react-native";
 import {deviceWidth} from "../constants";
 import RenderHTML from "react-native-render-html";
+import axios from "axios";
 
 export const Article = ({route, navigation})=>{
+    const [html, setHtml] = useState(route.params.html)
+    useEffect(()=>{
+        if(!route.params.html && route.params.url){
+            axios.get(route.params.url).then(res=>
+                setHtml(res.data.split("//").join("https://")
+                    .split("data-").join("")
+                    .split("http:https").join("https")
+                    .split("https:https").join("https")))
+        }
+    },[])
     return (
-        <ScrollView contentContainerStyle={{justifyContent:"center", alignItems:"center"}}>
-            <View style={{width:deviceWidth*0.95}}>
-                <RenderHTML source={{html:route.params.html}} contentWidth={deviceWidth*0.95}/>
-            </View>
-        </ScrollView>
+        <FlatList 
+            data={[]}
+            ListEmptyComponent={()=>(<View style={{width:deviceWidth*0.95}}>
+                <RenderHTML source={{"html": html}} contentWidth={deviceWidth*0.95}
+                            enableExperimentalGhostLinesPrevention={true}
+                            enableExperimentalMarginCollapsing={true}
+                            enableExperimentalBRCollapsing={true} ignoredDomTags={["canvas"]}/>
+            </View>)}
+            contentContainerStyle={{justifyContent:"center", alignItems:"center"}} renderItem={()=>null}/>
     )
 }
