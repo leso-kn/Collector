c9dd134d4b8928d314a62e14a2bc5bded75546b8
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2022-08-05 21:40:04 +0800

    chore: remove unused imports
    (cherry picked from commit d559370d2b168d828aed09ecdc37a0eef3e61bf4)

--- app/services/BiliArticle/Extractors/BiliArticleExtractor.js
+++ app/services/BiliArticle/Extractors/BiliArticleExtractor.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {articlePageUrl, channelInfoApiUrl, viewInfoApiUrl} from "../BiliArticleLinks";
+import {articlePageUrl} from "../BiliArticleLinks";
 import {shortenLargeNumber} from "../../../utils";
 import {mobileSpaceUrl} from "../../bilispace/BiliSpaceLinks";
 import {BiliPostExtractor} from "../../bilispace/Extractors/BiliPostExtractor";
