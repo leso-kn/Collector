4684c80c78bf9b376bf547d7ecda519315866d23
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2022-08-06 22:24:01 +0800

    feat: post extractor(Twitter)
    (cherry picked from commit 690243c8246167f07cea56af686d25c90f645b85)

---
+++ app/services/Twitter/Extractors/TwitterPostExtractor.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import {getFullTweetApiUrl, twitterUserPageUrl} from "../TwitterLinks";
+import {defaultParams, getToken, handleResult} from "../TwitterService";
+
+export class TwitterPostExtractor{
+    data
+    url
+    id
+    tweetData
+    userData
+    comments
+    constructor(url, id, data) {
+        this.url = url
+        if(data){
+            this.tweetData = data.tweetData
+            this.userData = data.userData
+            return this
+        }
+        else {
+            let requestParams = {
+                ...defaultParams,
+                'include_tweet_replies': '1',
+                'include_want_retweets': '1',
+            };
+            id = this.url.split('/')[this.url.split('/').length - 1].split("?")[0]
+            return (async () => await getToken()
+                .then(res=>axios.get(getFullTweetApiUrl + id + ".json", {params: requestParams, headers: res}))
+                .then(res=>{
+                    this.comments = res
+                    this.tweetData = res.data.globalObjects.tweets[id]
+                    this.userData = res.data.globalObjects.users[this.tweetData.user_id_str]
+                    return this
+                }))()
+        }
+    }
+    getServicePrefix(){
+        return "Twitter"
+    }
+    getName(){
+        return this.userData.name
+    }
+    getAvatar(){
+        return this.userData.profile_image_url_https
+    }
+    getSubName(){
+        return "@" + this.userData.screen_name
+    }
+    getIdentifyName(){
+        return this.userData.screen_name
+    }
+    getChannelIdentifyID(){
+        return "Twitter" + this.getIdentifyName()
+    }
+    getChannelUrl(){
+        return twitterUserPageUrl + this.userData.id_str
+    }
+    getUpvoteNum(){
+        return this.tweetData.favorite_count
+    }
+    getCommentNum(){
+        return this.tweetData.reply_count
+    }
+    getRepostNum(){
+        return this.tweetData.retweet_count
+    }
+    getPrefix() {
+        return null
+    }
+    getPubTime(){
+        return this.tweetData.created_at
+    }
+    getTitle(){
+        return null
+    }
+    getContent(){
+        return this.tweetData.full_text
+    }
+    getImages(){
+        return this.tweetData.extended_entities?.media?.map(x=>{return {uri:x.media_url_https}})
+    }
+    getHighlightUrl(){
+        return null
+    }
+    getRefPost(){
+        return null
+    }
+    getType(){
+        return null
+    }
+    getID(){
+        return this.tweetData.id_str
+    }
+    getIdentifyID(){
+        return this.getServicePrefix() + this.getID()
+    }
+    getParentID(){
+        return null
+    }
+    getParentType(){
+        return null
+    }
+    async getComments(pn, lastID){
+        if(this.comments){
+            return handleResult(this.comments, undefined, "getComments", [this.getID()])
+        }
+        return TwitterPostExtractor.getCommentsImpl(this.getID(), lastID)
+    }
+    static async getCommentsImpl(ID, parentID, lastID){
+        let requestParams = {
+            ...defaultParams,
+            'include_tweet_replies': '1',
+            'include_want_retweets': '1',
+            cursor:lastID
+        };
+        console.log(lastID)
+        return getToken()
+            .then(res=> {
+               return axios.get(getFullTweetApiUrl+ ID + ".json", {headers: res,  params:requestParams})
+            })
+            .then(res=>handleResult(res, undefined, "getComments", parentID?[ID, parentID]:[ID]))
+    }
+    getReposts(){
+        return null
+    }
+}
