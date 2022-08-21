15319e865e69820baa9263844c06a4436c0b8148
Infinity1309 <96324692+InfinityLoop1309@users.noreply.github.com>
2022-08-22 07:55:14 +0800

    refactor: add replaceAll

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-url-polyfill/auto';
-
+String.prototype.replaceAll = function(from, to){
+    let value = this
+    while(value.includes(from))value = value.replace(from,to)
+    return value.toString()
+}
 
 
 
